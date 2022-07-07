85116782a840b640bba15d9e26ad539fd1507d08
jaimik64 <jaimikchauhan5463@gmail.com>
2022-07-07 19:02:38 +0530

    Completed Section-11

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyACH0vSm6pvhUhxV8XFLtHtqZEmp9ijeJo';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+        type: 'video'
+    }
+});
+
