937ae049148560f9cf24f315a91f967369320e30
jaimik64 <jaimikchauhan5463@gmail.com>
2022-08-20 16:48:01 +0530

    Added Streams directory

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,77 @@
 import React from "react";
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/index'
 
-const StreamCreate = () => {
-    return (
-        <div>
-            Stream Create
-        </div>
-    )
+class StreamCreate extends React.Component {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    renderInput = ({ input, label, meta }) => {
+        const className = `field ${meta.error} && ${meta.touched} ? 'error' : '`
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+        )
+    }
+
+    onSubmit = (formValues) => {
+        console.log(formValues);
+        this.props.createStream(formValues)
+    }
+
+    render() {
+        return (
+            <form
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+                className="ui form error"
+            >
+                <Field
+                    name="title"
+                    label="Enter Title"
+                    component={this.renderInput}
+                />
+                <Field
+                    name="description"
+                    label="Enter Description"
+                    component={this.renderInput}
+                />
+                <button className="ui text primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+const validate = formValues => {
+
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'Title Can not be empty'
+    }
+
+    if (!formValues.description) {
+        errors.description = 'Description required'
+    }
+
+    return errors;
+}
+
+export default connect(null, { createStream })(
+    reduxForm({
+        form: 'streamCreate',
+        validate,
+    })(StreamCreate)
+)
---
+++ streams/client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form';
+import authReducers from './authReducers'
+
+
+export default combineReducers({
+    auth: authReducers,
+    form: formReducer
+})
